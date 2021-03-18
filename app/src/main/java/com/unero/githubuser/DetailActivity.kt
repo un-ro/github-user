2ff6b6596b9c74ad920bd041eda4e7ad1419dfd6
Unero <ramadhanbhg@gmail.com>
2021-03-18 22:07:12 +0700

    Layouting #4

--- app/src/main/java/com/unero/githubuser/DetailActivity.kt
+++ app/src/main/java/com/unero/githubuser/DetailActivity.kt
@@ -2,6 +2,7 @@ package com.unero.githubuser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.unero.githubuser.data.User
 import com.unero.githubuser.databinding.ActivityDetailBinding
 
@@ -22,12 +23,28 @@ class DetailActivity : AppCompatActivity() {
         binding.imgAvatar.setImageResource(user.avatar)
         binding.tvName.text = user.name
         binding.tvUsername.text = user.username
-        binding.tvloc.text = "Live at ${user.location}"
-        binding.tvrepo.text = "Repositories: ${user.repository}"
-        binding.tvcompany.text = "Work at ${user.company}"
+        binding.tvloc.text = "${user.location}"
+        binding.tvrepo.text = "${user.repository} Repositories"
+        binding.tvcompany.text = "${user.company}"
         binding.tvfollower.text = "Followed by ${user.followers} users"
         binding.tvfollowing.text = "Following ${user.following} users"
 
+        // Add icon to TextView
+        binding.tvloc.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_place_24, 0, 0, 0)
+        binding.tvcompany.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_work_24, 0, 0, 0)
+        binding.tvfollower.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_people_alt_24, 0, 0, 0)
+        binding.tvfollowing.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_people_alt_24, 0, 0, 0)
+        binding.tvrepo.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_folder_24, 0, 0, 0)
+
         setContentView(binding.root)
+
+        // Back button at ActionBar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "${user.username} Profile"
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
