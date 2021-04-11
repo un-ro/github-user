64364be6b66da304092af31a7c46b1d8d51c0919
Unero <ramadhanbhg@gmail.com>
2021-04-11 17:04:13 +0700

    Submission 3: Stack Widget (Dicoding)

---
+++ app/src/main/java/com/unero/githubuser/ui/widget/StackWidgetService.kt
@@ -0,0 +1,8 @@
+package com.unero.githubuser.ui.widget
+
+import android.content.Intent
+import android.widget.RemoteViewsService
+
+class StackWidgetService: RemoteViewsService() {
+    override fun onGetViewFactory(intent: Intent?): RemoteViewsFactory = StackRemoteViewsFactory(this.applicationContext)
+}
